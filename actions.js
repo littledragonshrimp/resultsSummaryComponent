c519d4bb9c7cfc9387a38a8d6087b251a7a7e104
Jordan <JDLChang@gmail.com>
2023-02-22 11:01:21 -0600

    adds updated js

--- actions.js
+++ actions.js
@@ -1,14 +1,32 @@
 //import data from './data.json' assert type {'json'};
 //console.log(data);
 
-fetch("./data.json")
-    .then(res => {
-        if (res.ok) {
-            console.log("SUCCESS")
-        } else{
-            console.log("Not Successful")
-        }
-    })
-    .then(data => console.log(data))
-    .catch(error => console.log("ERROR"))
+ fetch("./data.json")
+     .then(res => {
+         if (res.ok) {
+             console.log("SUCCESS")
+         } else{
+             console.log("Not Successful")
+         }
+     })
+     .then(data => console.log(data.json))
+     .catch(error => console.log("ERROR"))
 
+const category = JSON.parse(category.json[0]);
+console.log(category);
+
+
+// var data = JSON.parse(DATA);
+// 
+// var list = document.createElement("ul");
+// for (let i of data) { var item =
+// document.createElement("li"); list.appendChild(item)
+// }
+// 
+// document.getElementsById(ID).appendChild(list);
+
+// const data = require('./data.json');
+// 
+// console.log(data.category.map(()))
+// 
+// const category = console.log(data.category[1]);
